765f2d3aff63cd0c0eb58dffc23f78fbb4ae8c54
Detlef Lampart <detlef@lampart.de>
2024-02-12 21:29:18 +0100

    Changed ENTRYPOINT

--- config_files/auto_init.sh
+++ config_files/auto_init.sh
@@ -3,8 +3,6 @@ FIRST_CONFIG="/.FIRST_CONFIG"
 if [ ! -e $FIRST_CONFIG ]; then
     touch $FIRST_CONFIG
     /sbin/first_start.sh 
-    exit   
 else
     echo "Run /sbin/init directly"
-    exit
 fi
