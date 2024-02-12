dc03c971d09b50097ec55b4a45231a31937a51bd
Detlef Lampart <detlef@lampart.de>
2024-02-12 21:08:02 +0100

    Added exit to 'auto_init.sh'

--- config_files/auto_init.sh
+++ config_files/auto_init.sh
@@ -2,7 +2,9 @@
 FIRST_CONFIG="/.FIRST_CONFIG"
 if [ ! -e $FIRST_CONFIG ]; then
     touch $FIRST_CONFIG
-    /sbin/first_start.sh    
+    /sbin/first_start.sh 
+    exit   
 else
     echo "Run /sbin/init directly"
+    exit
 fi
